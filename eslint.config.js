4f689d53fca2e891e1e65cb4f7c0e9135c5edcf0
James Aprosail <aprosail@outlook.com>
2024-05-19 16:06:31 +0800

    eslint add some custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,13 @@ import eslintTS from "typescript-eslint"
 export default eslintTS.config(
   eslintJS.configs.recommended,
   ...eslintTS.configs.recommended,
+  ...eslintTS.configs.strict,
+  ...eslintTS.configs.stylistic,
+  {
+    rules: {
+      eqeqeq: 2,
+      "no-console": 2,
+      "no-lonely-if": 2,
+    },
+  },
 )
