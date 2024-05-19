d0761ac3989019889c7266bc7aea35239262e72d
James Aprosail <aprosail@outlook.com>
2024-05-19 18:31:47 +0800

    rollup rm unnecessary ts plugin

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -1,11 +1,10 @@
 import terser from "@rollup/plugin-terser"
-import typescript from "@rollup/plugin-typescript"
 import {defineConfig} from "rollup"
 import dts from "rollup-plugin-dts"
 
 export default defineConfig([
   {
-    plugins: [typescript(), terser({compress: true})],
+    plugins: [terser({compress: true})],
     input: "build/index.js",
     output: [
       {file: "index.js", format: "esm"},
