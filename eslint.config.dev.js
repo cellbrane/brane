a66525770b346971851b9f9df0d145e6003da640
James Aprosail <aprosail@outlook.com>
2024-05-19 17:16:33 +0800

    eslint simplify code about freeze

--- eslint.config.dev.js
+++ eslint.config.dev.js
@@ -1,24 +1,11 @@
 import configs from "./eslint.config.js"
 
 // Turn all errors into warns to distinguish from language level errors.
-const handler = []
-for (const config of configs) {
-  const configHandler = {}
-  for (const key of Object.keys(config)) {
-    if (key !== "rules") {
-      configHandler[key] = config[key]
-      continue
-    }
-    const rules = {}
-    for (const rule of Object.keys(config.rules)) {
-      rules[rule] =
-        [2, "error"].indexOf(config.rules[rule]) === -1
-          ? config.rules[rule]
-          : "warn"
-    }
-    configHandler["rules"] = rules
+export default configs.map(function (config) {
+  // Raw rules are freezed.
+  const rules = (config.rules = {...config.rules})
+  for (const rule of Object.keys(rules)) {
+    if ([2, "error"].indexOf(rules[rule]) !== -1) rules[rule] = "warn"
   }
-  handler.push(configHandler)
-}
-
-export default handler
+  return config
+})
