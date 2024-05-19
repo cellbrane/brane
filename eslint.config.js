0191064a1064a2845222e4b802e0999653277b5c
James Aprosail <aprosail@outlook.com>
2024-05-19 15:18:06 +0800

    eslint rm unnecessary avoid prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,7 @@
 import eslintJS from "@eslint/js"
-import prettier from "eslint-config-prettier"
 import eslintTS from "typescript-eslint"
 
 export default eslintTS.config(
-  // Basic recommended JS and TS lint rules.
   eslintJS.configs.recommended,
   ...eslintTS.configs.recommended,
-
-  // Cancel all style lints and let prettier do.
-  prettier,
 )
